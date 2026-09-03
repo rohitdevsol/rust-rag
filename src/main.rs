bbbdc2d6e1a1b9d87b619c0861fd3eb36a08a9a6
Rohit <rohitnarnolia88@gmail.com>
2026-09-03 18:25:19 +0530

    retrieve related chunks using cosine similarity

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-use naive_rag::{chunks_to_embeddings, make_chunks};
+use std::io::{self, Write};
+
+use naive_rag::{chunks_to_embeddings, cosine_similarity, make_chunks, query_to_embeddings};
 fn main() {
     let file = std::fs::read_to_string("./document.txt").unwrap();
 
-    let chunks = make_chunks(file, 20);
+    let chunks = make_chunks(file, 50);
 
-    let embeddings = chunks_to_embeddings(chunks);
+    let embeddings = chunks_to_embeddings(&chunks);
 
-    for embedding in embeddings {
-        println!("Dimensions: {}", embedding.len());
+    print!("Enter your query: ");
+    io::stdout().flush().unwrap();
+
+    let mut query = String::new();
+
+    io::stdin().read_line(&mut query).unwrap();
+
+    let query_embedding = query_to_embeddings(query);
+
+    let mut res: Vec<(f32, String)> = Vec::new();
+
+    for (chunk, embedding) in chunks.iter().zip(embeddings.iter()) {
+        let score = cosine_similarity(&query_embedding, &embedding);
+        res.push((score, chunk.clone()));
+        // println!("Score is {}", score)
+    }
+
+    res.sort_by(|a, b| b.0.partial_cmp(&a.0).unwrap());
+
+    for record in res.iter().take(3) {
+        println!("Score: {}", record.0);
+        println!("Chunk: {}", record.1);
+        println!("----------------");
+        println!("                ");
     }
 }
