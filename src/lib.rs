274424a6d72e67249de21b03155bd7ab1e2094bd
Rohit <rohitnarnolia88@gmail.com>
2026-09-03 18:24:38 +0530

    add cosine similarity

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use fastembed::{EmbeddingModel, TextEmbedding, TextInitOptions};
 
 pub fn make_chunks(file: String, chunk_size: usize) -> Vec<String> {
-    // itertate over this file and every 10 chars split and store that much owned string in the Vec
     let mut vec = Vec::new();
 
     let mut prev = 0;
@@ -18,23 +17,43 @@ pub fn make_chunks(file: String, chunk_size: usize) -> Vec<String> {
             next = next + 1;
         }
     }
-
     vec
 }
 
-pub fn chunks_to_embeddings(chunks: Vec<String>) -> Vec<Vec<f32>> {
+pub fn chunks_to_embeddings(chunks: &Vec<String>) -> Vec<Vec<f32>> {
+    println!("Loading embedding model...");
+
     let mut model =
         TextEmbedding::try_new(TextInitOptions::new(EmbeddingModel::AllMiniLML6V2)).unwrap();
 
+    println!("Embedding model loaded!");
     let embeddings = model.embed(chunks, None).unwrap();
+
+    println!("Embeddings generated!");
     embeddings
 }
 
-pub fn query_to_embeddings(query: String) -> Vec<Vec<f32>> {
-    let mut v = Vec::new();
-    v.push(query);
+pub fn query_to_embeddings(query: String) -> Vec<f32> {
+    let mut model =
+        TextEmbedding::try_new(TextInitOptions::new(EmbeddingModel::AllMiniLML6V2)).unwrap();
 
-    chunks_to_embeddings(v)
+    model.embed(vec![query], None).unwrap().swap_remove(0)
+}
+
+pub fn cosine_similarity(a: &[f32], b: &[f32]) -> f32 {
+    assert_eq!(a.len(), b.len());
+
+    let mut dot_product = 0.0;
+    let mut magnitude_a = 0.0;
+    let mut magnitude_b = 0.0;
+
+    for i in 0..a.len() {
+        dot_product += a[i] * b[i];
+        magnitude_a += a[i] * a[i];
+        magnitude_b += b[i] * b[i];
+    }
+
+    dot_product / (magnitude_a.sqrt()) * (magnitude_b.sqrt())
 }
 
 #[cfg(test)]
@@ -54,9 +73,18 @@ pub fn test_embeddings_from_chunks() {
     let st = String::from("Hello how are you doing hope you are doing fine");
     let chunks = make_chunks(st, 12);
 
-    let embeddings = chunks_to_embeddings(chunks);
+    let embeddings = chunks_to_embeddings(&chunks);
 
     for embedding in embeddings {
         println!("Dimensions: {}", embedding.len());
     }
+}
+
+#[test]
+pub fn test_cosine_smimilarity() {
+    let a = vec![1.0, 0.0];
+    let b = vec![0.0, 0.0];
+
+    let score = cosine_similarity(&a, &b);
+    println!("Score is: {score}");
 }
